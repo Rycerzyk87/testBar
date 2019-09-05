48b00f6ef8d9baa36ac9fb6dc37aa9b8cc917ba5
Adam Samulski <a.samulski@op.pl>
2019-09-05 17:13:08 +0200

    addevent to buttons

--- widget.js
+++ widget.js
@@ -1,27 +1,34 @@
-// let myBar = new Bar({
-//     message: 'Add beautiful widgets to your website',
-//     position: 'top',
-// });
 
-// const btn = document.querySelector(".show a");
-// const bar = document.querySelector(".visible");
-// const text = document.querySelector(".text");
-// const close = document.querySelector(".close");
-
-// const heigth = myBar.position;
-
-// btn.addEventListener("click", () => {
-//     console.log("dzia");
-// })
+startBtn = document.querySelector(".show a");
+textArea = document.querySelector(".text");
+closeBtn = document.querySelector(".close");
+bar = document.querySelector(".visible");
+getWidgets = document.querySelector(".barCenter button");
 
 class Bar {
     constructor(props) {
-        this.startBtn = document.querySelector(".show a");
-        this.textArea = document.querySelector(".text");
-        this.closeBtn = document.querySelector(".close");
-        this.bar = document.querySelector(".visible");
-
         this.txt = props.message;
         this.position = props.position;
     }
+
+
 }
+getWidgets.addEventListener("click", () => {
+    alert("hello world");
+})
+
+startBtn.addEventListener("click", () => {
+    textArea.innerHTML = myBar.txt;
+    bar.style.display = 'block';
+})
+
+closeBtn.addEventListener("click", () => {
+    bar.style.display = 'none';
+})
+
+
+
+let myBar = new Bar({
+    message: 'Add beautiful widgets to your website',
+    position: 'top',
+});
